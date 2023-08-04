21017d7df7e28de8bdbe936079b576a063b48a7d
888Leonidas888 <jhonny14_1@hotmail.com>
2023-08-03 17:12:25 -0700

    Se crea ejecutable 1-last_digit 1-last_digit.c

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+	int last_n;
+	const char *STR_LAST = "Last digit of";
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last_n = n % 10;
+
+	if (last_n > 5)
+		printf("%s %d is %d and is greater then 5\n", STR_LAST, n, last_n);
+	else if (last_n == 0)
+		printf("%s %d is %d and is 0\n", STR_LAST, n, last_n);
+	else if (last_n < 6 && last_n != 0)
+		printf("%s %d is %d and is less than 6 and not 0\n", STR_LAST, n, last_n);
+	return (0);
+}
