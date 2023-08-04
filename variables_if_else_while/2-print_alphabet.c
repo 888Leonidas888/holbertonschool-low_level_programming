5686156024e6c432a4ed68d9c9736c1239caf733
888Leonidas888 <jhonny14_1@hotmail.com>
2023-08-04 09:17:26 -0700

    se crea archivo 2-print_alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int c;
+	int LF = 10;
+
+	for (c = 97 ; c <= 122 ; c++)
+		putchar(c);
+	putchar(LF);
+	getchar();
+	return (0);
+}
