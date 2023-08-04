15dfa737729c0f4e351d2518c147c5ae00b92b11
888Leonidas888 <jhonny14_1@hotmail.com>
2023-08-03 17:21:02 -0700

    se actuliza codigo fuente 1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+#include <math.h>
 
 /**
  *main - Entry Point
@@ -16,7 +17,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	last_n = n % 10;
+	last_n = abs(n) % 10;
 
 	if (last_n > 5)
 		printf("%s %d is %d and is greater then 5\n", STR_LAST, n, last_n);
