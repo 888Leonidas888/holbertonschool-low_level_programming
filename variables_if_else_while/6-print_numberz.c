511b0fbad2cb336fbb2fc0b96906243db9091e59
888Leonidas888 <jhonny14_1@hotmail.com>
2023-08-04 09:57:09 -0700

    se crea archivo 6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48 ; n <= 57 ; n++)
+		putchar(n);
+	putchar(10);
+	getchar();
+	return (0);
+}
