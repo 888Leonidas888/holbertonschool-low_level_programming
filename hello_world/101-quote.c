141768df138031c3a7cec34cfddd611204c2d84d
888Leonidas888 <jhonny14_1@hotmail.com>
2023-08-06 18:14:56 -0700

    se crea archivo 101-quote.c

---
+++ hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+/**
+ *main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	system("echo and that piece of art is useful\\\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
