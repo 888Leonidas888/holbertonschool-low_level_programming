1c21174c7de6fe4fbf7dc7d0b58c78e9d603bc00
888Leonidas888 <jhonny14_1@hotmail.com>
2023-08-10 10:20:02 -0700

    se corrige archivo 7-print_last_digit.c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -12,9 +12,9 @@ int print_last_digit(int n)
 	int last_digit;
 	int nn;
 
-	nn = abs(n);
+	last_digit = n % 10;
+	nn = abs(last_digit);
 
-	last_digit = nn % 10;
-	_putchar(last_digit + '0');
-	return (last_digit);
+	_putchar(nn + '0');
+	return (nn);
 }
