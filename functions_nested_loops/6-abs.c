a23f8951571c00d5893ff85d4deac0aa51d62a77
888Leonidas888 <jhonny14_1@hotmail.com>
2023-08-09 17:19:41 -0700

    se crea archivo 6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _abs -  Return the value absolute.
+ * @int: Recive a number.
+ *
+ * Return: Is returned value abs.
+ */
+
+int _abs(int n)
+{
+	return (abs(n));
+}
