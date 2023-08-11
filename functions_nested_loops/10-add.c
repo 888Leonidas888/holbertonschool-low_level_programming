beeb6358e1119cc140afefe558b6ff9a9322d73c
888Leonidas888 <jhonny14_1@hotmail.com>
2023-08-11 11:40:13 -0700

    se crea archivo 10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Sum two numbers.
+ * @a: Number int.
+ * @b: Number int.
+ *
+ * Return: Returns the sum of its arguments.
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
