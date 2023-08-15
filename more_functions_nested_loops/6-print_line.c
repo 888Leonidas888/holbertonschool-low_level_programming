3930ccbfaf1fac77ffab9df3b08e061cf6f7d131
888Leonidas888 <jhonny14_1@hotmail.com>
2023-08-15 12:04:51 -0700

    se crea programa que imprime _ la cantidad de veces pasada en su argumento.

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_liner - Print in stdout a line.
+ * @n: integer, print times the sign _
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	const int LINER = 95;
+	int i = 0;
+
+	if (n > 0)
+		while (i < n)
+		{
+			_putchar(LINER);
+			i++;
+		}
+	_putchar('\n');
+}
