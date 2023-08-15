9c5e306ecdf8017ab1504dd321161df2d905a7c9
888Leonidas888 <jhonny14_1@hotmail.com>
2023-08-15 08:28:23 -0700

    se crea programa que multiplica dos números

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplication two numbers.
+ * @a: Integer.
+ * @b: Integer.
+ *
+ * Return: the product de two integer.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
