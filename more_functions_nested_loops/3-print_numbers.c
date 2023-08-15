f94b0ce2b0330ddcd124e7d7d4e43118fa88d52f
888Leonidas888 <jhonny14_1@hotmail.com>
2023-08-15 09:08:09 -0700

    se crea programa que imprime los números del 0 al 9.

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print stdout the numbres of 0 to 9.
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		_putchar(i);
+	_putchar('\n');
+}
