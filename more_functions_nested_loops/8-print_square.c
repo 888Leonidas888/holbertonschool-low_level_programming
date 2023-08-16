5f696d5fccff2bfdabada1da371ac00ea66f9fde
888Leonidas888 <jhonny14_1@hotmail.com>
2023-08-15 19:13:12 -0700

    Este programa imprime la forma de un cuadrado usando el signo del gato para ello.

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square -  Print in stdout a square.
+ * @size: Height of square.
+ *
+ * Return: void
+ */
+void print_square(int size)
+{
+	const int MICHI = 35;
+	const int NEW_LINE = 10;
+	int i, o;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (o = 1; o <= size; o++)
+					_putchar(MICHI);
+			_putchar(NEW_LINE);
+		}
+	} else
+		_putchar(NEW_LINE);
+}
