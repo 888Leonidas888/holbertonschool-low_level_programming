8f112a51c13a78bac25585cd5ef798bae04f3398
888Leonidas888 <jhonny14_1@hotmail.com>
2023-08-17 08:17:45 -0700

    se crea programa que retorne el largo de una cadena.

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - Print in stdout the len of string.
+ * @s: Value a Read len.
+ *
+ * Return: the len of string.
+ */
+int _strlen(char *s)
+{
+	size_t len = strlen(s);
+
+	return (len);
+}
