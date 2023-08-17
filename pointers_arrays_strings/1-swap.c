503b0f377b4fe7f30a6cd6c556e5cd0a7482ebcc
888Leonidas888 <jhonny14_1@hotmail.com>
2023-08-17 07:51:24 -0700

    se crea programa que intercambia valores de sus parametros.

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - waps the values of two integers.
+ * @a: first integer.
+ * @b: second integer
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+
+	*a = *b;
+	*b = c;
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -4,5 +4,6 @@
 #include <unistd.h>
 
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 
 #endif
