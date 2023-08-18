aa2068201c0a5f66029a9f7ed665b1420e70285e
888Leonidas888 <jhonny14_1@hotmail.com>
2023-08-18 16:19:42 -0700

    programa que imprime cada 2 caracteres empezando desde el primer caracter.

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts2 - Printin stdout for two character. starting the first character.
+ * @str: String a print.
+ *
+ * Return: void.
+ */
+void puts2(char *str)
+{
+	int i;
+	int o = 1;
+	const int NEW_LINE = 10;
+
+	i = strlen(str) - 1;
+
+	putchar(str[0]);
+
+	while (o <= i)
+	{
+		if (o % 2 == 0)
+			putchar(str[o]);
+		o++;
+	}
+	putchar(NEW_LINE);
+}
