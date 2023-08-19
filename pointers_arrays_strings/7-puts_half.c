c9a84abf2dfb5c2bc0e24539ac563ca07e527325
888Leonidas888 <jhonny14_1@hotmail.com>
2023-08-18 17:10:24 -0700

    programa que imprime la segunda mitad de un cadena.

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * puts_half - Print in stdout, the half of a string.
+ * @str: String to print.
+ *
+ * Return: void.
+ */
+void puts_half(char *str)
+{
+	int len, start;
+	const int NEW_LINE = 10;
+
+	len = strlen(str);
+	
+	if (len != 0)
+	{
+		if (len % 2 == 0)
+			start = len / 2;
+		else
+			start = (len - 1 / 2) + 1;
+
+		len--;
+
+		while ( start <= len)
+		{
+			putchar(str[start]);
+			start++;
+		}
+	}
+	putchar(NEW_LINE);
+
+
+}
