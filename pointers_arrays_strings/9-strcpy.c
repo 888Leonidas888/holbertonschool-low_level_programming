45bb52901d9c09466f61c845ff6c91eae6fbd99f
888Leonidas888 <jhonny14_1@hotmail.com>
2023-08-19 17:25:33 -0700

    Este programa copia una cadena un buffer y retorna el puntero.

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copy string to buffer.
+ * @dest: Is the buffer
+ * @src: Source of string.
+ *
+ * Return: The pointer to dest.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (dest);
+}
