bd7c996fa216ed24b59a90801213f7fee7ee4203
888Leonidas888 <jhonny14_1@hotmail.com>
2023-08-21 13:30:07 -0700

    Este programa retorna la cantidad de argumentos.

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, This program print in stdout the number of arguments.
+ * @argc: Number og arguments
+ * @argv: Arguments send our program.
+ *
+ * Return: Success 0.
+ */
+int main(int argc, char const *argv[] __attribute__((unused)))
+{
+	printf("%d\n",argc - 1);
+	return (0);
+}
