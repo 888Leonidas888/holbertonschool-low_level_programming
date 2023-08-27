26c28157da609c377d8627fd666b63127d08399a
888Leonidas888 <jhonny14_1@hotmail.com>
2023-08-27 03:04:36 -0700

    Se corrige función

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -14,8 +14,16 @@ char *str_concat(char *s1, char *s2)
 	int o = 0;
 	char *ptr;
 
-	len1 = strlen(s1);
-	len2 = strlen(s2);
+	if (s1 == NULL)
+		len1 = 1;
+	else
+		len1 = strlen(s1);
+
+	if (s2 == NULL)
+		len2 = 1;
+	else
+		len2 = strlen(s2);
+
 	maxLen = len1 + len2 + 1;
 
 	ptr = malloc(sizeof(char) * maxLen);
