8aa9ed8f18d93946efe29a567ed056124c4ffa78
888Leonidas888 <jhonny14_1@hotmail.com>
2023-08-28 15:19:21 -0700

    Esta función imprime recibe un structura ya inicializada y la imprime.

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - This function print a struct.
+ * @d: Received a struct initialized.
+ *
+ * Return: void.
+ */
+void print_dog(struct dog *d)
+{
+	printf("Name: %s\n", (*d).name);
+	printf("Age: %f\n", (*d).age);
+	printf("Owner: %s\n", (*d).owner);
+
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -16,5 +16,24 @@ typedef struct dog
 	char *owner;
 } dog;
 
+
+
+/**
+ * struct dog_t - Structut for created a dog.
+ *@name: Name of dog.
+ *@age: Age of dog.
+ *@owner: Owner of dog.
+ */
+typedef struct dog_t
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 #endif
