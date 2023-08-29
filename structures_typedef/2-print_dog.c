b6c4fcfc55bbed518737a37a4c722d3915b0d1a6
888Leonidas888 <jhonny14_1@hotmail.com>
2023-08-28 20:58:04 -0700

    se actualiza función.

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -11,9 +11,16 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
+	if ((*d).name == NULL)
+		printf("Name:(%s)\n", (*d).name);
+	else
+		printf("Name: %s\n", (*d).name);
 
-	printf("Name: %s\n", (*d).name);
 	printf("Age: %f\n", (*d).age);
-	printf("Owner: %s\n", (*d).owner);
+
+	if ((*d).owner == NULL)
+		printf("Owner: (%s)\n", (*d).owner);
+	else
+		printf("Owner: %s\n", (*d).owner);
 
 }
