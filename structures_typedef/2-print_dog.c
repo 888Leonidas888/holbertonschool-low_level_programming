a2f2efafb4696c60119cc541589ea0c696619098
Jhony Reder Escriba Bautista <98334376+888Leonidas888@users.noreply.github.com>
2023-08-28 23:05:46 -0500

    Se actualiza 2-print_dog.c

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -14,7 +14,7 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	if ((*d).name == NULL)
-		printf("Name:(%s)\n", NIL);
+		printf("Name: (%s)\n", NIL);
 	else
 		printf("Name: %s\n", (*d).name);
 
