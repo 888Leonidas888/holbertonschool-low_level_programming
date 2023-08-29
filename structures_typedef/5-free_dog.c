390c3d19d3383c1fca5f52e70676168c1e1b6242
888Leonidas888 <jhonny14_1@hotmail.com>
2023-08-28 22:31:57 -0700

    Esta funciom sirve para liberar memoria pasandole un puntero a una estructura del tipo dog_t o dog

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+
+/**
+ * free_dog - Clean memory.
+ * @d: Pointer to struct.
+ *
+ * Return: void.
+ */
+void free_dog(dog_t *d)
+{
+	free(d.name);
+	free(d.owner);
+	free(d);
+}
