d78fc737a754bde5d4e7fa01d1e8eb91e972cb82
Jhony Reder Escriba Bautista <98334376+888Leonidas888@users.noreply.github.com>
2023-08-29 01:38:19 -0500

    Se corrige conflictos en el merge 5-free_dog.c

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -8,16 +8,10 @@
  */
 void free_dog(dog_t *d)
 {
-<<<<<<< HEAD
 	if (d != NULL)
 	{
 		free(d.name);
 		free(d.owner);
 		free(d);
 	}
-=======
-	free((*d).name);
-	free((*d).owner);
-	free(d);
->>>>>>> 8e36fea54fd90148f7fbdfc4886921e92368e10a
 }
