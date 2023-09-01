b6bf94ef362012184346b346829fcb3b5b4328f4
888Leonidas888 <jhonny14_1@hotmail.com>
2023-09-01 08:08:17 -0700

    Esta función ejecuta una función que recibe un numero de un array para imprimirlo en la salida standar 1-array_iterator.c

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - This function executes a function given as a parameter on each element of an array.
+ * @array: Array of number.
+ * @size: Size of array.
+ * @action: Function to a execute.
+ *
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i = 0;
+
+	while (i < size)
+	{
+		(*action)(array[i]);
+		i++;
+	}
+}
