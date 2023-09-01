a9b6388c2ef9eb117da6977d8a5171f03c001214
888Leonidas888 <jhonny14_1@hotmail.com>
2023-09-01 08:49:21 -0700

    Esta función retorna el indice dentro de array de un número dado.

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - This function return the index of number.
+ * @array: Array of number a check.
+ * @size: Size of array.
+ * @cmp: Function to execute.
+ *
+ * Return: Index of element match, otherwise -1.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0)
+		return (-1);
+
+	while (i < size)
+	{
+		if ((*cmp)(array[i]) != 0)
+			return (i);
+		i++;
+	}
+
+	return (-1);
+}
