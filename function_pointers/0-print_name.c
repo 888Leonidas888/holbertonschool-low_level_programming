81bfc0d26bab05dfa92b262336dd309f5bcf2681
888Leonidas888 <jhonny14_1@hotmail.com>
2023-09-01 06:47:19 -0700

    Se corrige error en función 0-print_name.c

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -11,9 +11,8 @@ void print_name_uppercase(char *name);
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (f != NULL)
-	{
-		f = &print_name_uppercase;
-		(*f)(name);
-	}
+	if (f == NULL)
+		return;
+	f = &print_name_uppercase;
+	(*f)(name);
 }
