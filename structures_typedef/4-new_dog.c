a5d457b0c216ed4c6af6b892ff94f2f2b7fe7aa3
888Leonidas888 <jhonny14_1@hotmail.com>
2023-09-02 11:00:53 -0700

    Se corrige función 4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,5 +1,4 @@
 #include "dog.h"
-
 /**
  * new_dog - Initialize a struct dog_t.
  * @name:  Name
@@ -27,15 +26,22 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_name = (char *)malloc(sizeof(char) * lenName);
 
 	if (new_name == NULL)
+	{
+		free(d);
 		return (NULL);
+	}
+
 	strcpy(new_name, name);
 
 	new_owner = (char *)malloc(sizeof(char) * lenOwner);
 
 	if (new_owner == NULL)
+	{
+		free(d);
 		return (NULL);
+	}
 	strcpy(new_owner, owner);
-	
+
 	(*d).name = new_name;
 	(*d).age = age;
 	(*d).owner = new_owner;
