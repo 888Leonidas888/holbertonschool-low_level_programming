5cdaceb9cce47a94b101294c1e216f103686668b
888Leonidas888 <jhonny14_1@hotmail.com>
2023-09-05 14:39:21 -0700

    Esta función imprime una cadena 2-print_strings.c

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,26 @@
+#include "varidic_functions.h"
+
+/**
+ * print_strings - Prints strings.
+ * @separator: Sepator to string.
+ * @n: Numbers of args.
+ *
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list listName;
+	unsigned int i;
+
+	va_start(listName, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (separator == NULL || i == n - 1)
+			printf("%s", va_arg(listName, char *);
+		else
+			printf("%s%s ", va_arg(listName, char *), operator);
+	}
+	putchar(10);
+	va_end(listName);
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -3,8 +3,11 @@
 
 #include <stdarg.h>
 #include <stdio.h>
+#include <stddef.h>
+#include <stdio.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 
 #endif
