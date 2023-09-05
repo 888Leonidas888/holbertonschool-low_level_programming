712e04b0f332198d83faa948f9e083944591534e
888Leonidas888 <jhonny14_1@hotmail.com>
2023-09-05 14:46:44 -0700

    se corrige 2-print_strings.c

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -19,7 +19,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (separator == NULL || i == n - 1)
 			printf("%s", va_arg(listName, char *));
 		else
-			printf("%s%s ", va_arg(listName, char *), separator);
+			printf("%s%s", va_arg(listName, char *), separator);
 	}
 	putchar(10);
 	va_end(listName);
