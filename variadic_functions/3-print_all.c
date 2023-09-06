72b4ed4f07ccddf97bc5af697e0c3b4e8e30f047
Jhony Reder Escriba Bautista <98334376+888Leonidas888@users.noreply.github.com>
2023-09-05 23:41:59 -0500

    Se corrige función 3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -9,23 +9,52 @@
 void print_all(const char * const format, ...)
 {
 	va_list listAll;
-	unsigned int i;
-	unsigned int n = 3;
-	char *str;
-
-	va_start(listAll, n);
-
-	for (i = 0; i < n; i++)
+	int i = 0;
+	int type;
+	char *c;
+	int o;
+	
+	va_start(listAll, format);
+	
+	if (format == NULL || listAll == NULL)
+		return;
+	
+	while (format[i] != '\0')
 	{
-		str = va_arg(listAll, char *);
-
-		if (str == NULL)
-			str = "(nil)";
-
-		if (i == 2)
-			printf("%s", str);
-		else
-			printf("%s ,", str);
+		type = format[i];
+		o = 0;
+		
+		switch (type)
+		{
+			case 'c':
+				printf("%c", va_arg(listAll, int));
+				o = 1;
+				break;
+			case 'i':
+				printf("%d", va_arg(listAll, int));
+				o = 1;
+				break;
+			case 'f':
+				printf("%f", va_arg(listAll, double));
+				o = 1;
+				break;
+			case 's':
+				c = va_arg(listAll, char *);
+				
+				if (c == NULL)
+					c = "(nil)";
+				
+				printf("%s", c);
+				o = 1;
+				break;
+			default:
+				break;
+		}
+	
+	
+		if (format[i + 1] != '\0' && o == 1)
+			printf("%s", ", ");
+		i++;
 	}
 	putchar(10);
 	va_end(listAll);
