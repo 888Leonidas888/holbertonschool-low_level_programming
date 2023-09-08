35b9f8106c29767c489159a8ec9d17aab68e9072
Jhony Reder Escriba Bautista <98334376+888Leonidas888@users.noreply.github.com>
2023-09-08 00:50:55 -0500

    Se actualiza 3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -9,49 +9,48 @@
 void print_all(const char * const format, ...)
 {
 	va_list listAll;
-	int i = 0;
-	int type;
+	int i = 0; a = 0;
+	int type, o;
 	char *c;
-	int o;
 
 	va_start(listAll, format);
 
-	if (format == NULL || listAll == NULL)
-		return;
-
-	while (format[i] != '\0')
+	while (format != NULL && listAll != NULL && a == 0)
 	{
-		type = format[i];
-		o = 0;
-
-		switch (type)
+		while (format[i] != '\0')
 		{
-			case 'c':
-				printf("%c", va_arg(listAll, int));
-				o = 1;
-				break;
-			case 'i':
-				printf("%d", va_arg(listAll, int));
-				o = 1;
-				break;
-			case 'f':
-				printf("%f", va_arg(listAll, double));
-				o = 1;
-				break;
-			case 's':
-				c = va_arg(listAll, char *);
-
-				printf("%s", c);
-				o = 1;
-				break;
-			default:
-				break;
+			type = format[i];
+			o = 0;	
+			switch (type)
+			{
+				case 'c':
+					printf("%c", va_arg(listAll, int));
+					o = 1;
+					break;
+				case 'i':
+					printf("%d", va_arg(listAll, int));
+					o = 1;
+					break;
+				case 'f':
+					printf("%f", va_arg(listAll, double));
+					o = 1;
+					break;
+				case 's':
+					c = va_arg(listAll, char *);
+					if (c == NULL)
+						c = "(nil)";
+					printf("%s", c);
+					o = 1;
+					break;
+				default:
+					break;
+			}	
+			if (format[i + 1] != '\0' && o == 1)
+				printf("%s", ", ");
+			i++;
 		}
-
-		if (format[i + 1] != '\0' && o == 1)
-			printf("%s", ", ");
-		i++;
+		putchar(10);
+		va_end(listAll);
+		a++;
 	}
-	putchar(10);
-	va_end(listAll);
 }
