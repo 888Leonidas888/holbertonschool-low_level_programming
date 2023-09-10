1cce3e0764eda6e09ca958201a1e95be5e4b2c82
Jhony Reder Escriba Bautista <98334376+888Leonidas888@users.noreply.github.com>
2023-09-10 12:24:55 -0500

    Update 5-rev_string.c

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -10,22 +10,22 @@ void rev_string(char *s)
 {
 	char *buffer;
 	int lenTxt, lenTmp, i;
-	
+
 	lenTxt = strlen(s);
-	
+
 	buffer = (char *)malloc(sizeof(char) * lenTxt);
-	
+
 	strcpy(buffer, s);
 	buffer[lenTxt] = '\0';
 
 	lenTmp = lenTxt - 1;
-	
+
 	for (i = 0; buffer[i] != '\0'; i++)
 	{
 		s[lenTmp] = buffer[i];
 		lenTmp--;
 	}
-	
+
 	free(buffer);
 	s[lenTxt] = '\0';
 }
