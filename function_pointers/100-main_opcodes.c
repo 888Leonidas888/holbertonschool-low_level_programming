de68d675db57201f64beb6ab3c25a47c33fbacab
888Leonidas888 <jhonny14_1@hotmail.com>
2023-09-10 21:24:17 -0700

    correccion

--- function_pointers/100-main_opcodes.c
+++ function_pointers/100-main_opcodes.c
@@ -2,6 +2,15 @@
 #include <stdio.h>
 
 /**
+ * function_main - Function of demo
+ *
+ * Return: void
+ */
+void function_main(void)
+{
+}
+
+/**
  * main - This program print is opcode.
  * @argc: Numbers of args, only two args accept.
  * @argv: Numbers bytes to print.
@@ -10,6 +19,8 @@
  */
 int main(int argc, char *argv[])
 {
+	unsigned char *ptr = (unsigned char *)&function_main;
+
 	if (argc != 2)
 	{
 		puts("Error");
@@ -22,5 +33,10 @@ int main(int argc, char *argv[])
 		return (2);
 	}
 
+	for (int i = 0; i < sizeof(function_main; i++))
+	{
+		printf("%02X ", ptr[i]);
+	}
+	putchar(10);
 	return (0);
 }
